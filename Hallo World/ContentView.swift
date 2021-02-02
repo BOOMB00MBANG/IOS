b4193803af6607e991895ddb07a3820dfa072e32
YHC <bityhc@sina.com>
2021-02-02 09:14:39 +0800

    https://github.com/BOOMB00MBANG/IOS.git

---
+++ Hallo World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hallo World
+//
+//  Created by 尹宏晨 on 2021/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hallo World/Hallo_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hallo_WorldApp.swift
+//  Hallo World
+//
+//  Created by 尹宏晨 on 2021/2/1.
+//
+
+import SwiftUI
+
+@main
+struct Hallo_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
